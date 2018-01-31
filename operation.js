cbdbc8efbcd527801f49e9c5cef43e2018a5972d
ROllens <zongtang0816@gmail.com>
2018-01-31 10:59:17 +0800

    2-1

--- operation.js
+++ operation.js
@@ -5,6 +5,12 @@ var app = new Vue({
         OAO:"OBO"
     }
 });
+var S1 = new Vue({
+    el:'#Section1',
+    data:{
+        text:["面對大眾運輸越來越完備的台灣","轉乘規劃總是旅人在行程中最頭痛的","對此，眾多平台已經推出相關的「全套導航」","讓人們像是包裹一樣隨著固定的路線運送","Translist致力於讓旅人們","不僅能得到相關的轉乘資訊","更能依照自己的需求改變任何旅途中的班次","最後再輸出成卡片清單","智慧規劃、輕鬆搭乘","Translist會帶給你新的規劃與搭乘的體驗"]
+    }
+});
 var Homegb = ["url(source/img/christophe-maertens-463540.jpg)","url(source/img/stefan-kunze-24548)","url(source/img/c-chapman-384658)"]
 var counter = 0 ; 
 function HomeBackground()
